e3bc41dd434093b832ec07c3691bdb190b7a7e99
Alexandre Vidal <161221123+arvidalpython@users.noreply.github.com>
2025-11-25 17:19:04 -0300

    fix: correção de erros nos serializers e models v2

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('', RedirectView.as_view(url='/api/v1/')),
+    re_path('bookstore/(?P<version>(v1lv2))/', include('order.urls')),
+    re_path('bookstore/(?P<version>(v1lv2))/', include('product.urls'))
 ]
--- order/models/order.py
+++ order/models/order.py
@@ -4,11 +4,8 @@ from product.models.product import Product
 from django.conf import settings
 
 class Order(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total = models.DecimalField(max_digits=10, decimal_places=2)
 
